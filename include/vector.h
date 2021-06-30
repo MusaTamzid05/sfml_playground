1563a85c847ba49116ca24204a80c8673d61f834
MusaTamzid05 <tamzid101@gmail.com>
2021-06-30 21:25:09 +0600

    vector added.

---
+++ include/vector.h
@@ -0,0 +1,13 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+namespace Engine {
+    struct Vector2 {
+        Vector2(float x, float y);
+        float x;
+        float y;
+
+    };
+}
+
+#endif
---
+++ src/vector.cpp
@@ -0,0 +1,5 @@
+#include "vector.h"
+
+namespace Engine {
+    Vector2::Vector2(float x, float y):x(x), y(y) {}
+}
